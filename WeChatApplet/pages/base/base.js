d13549c89bcb4c62ace35fdb4df4ba870b8b6bee
张仁山 <1041369205@qq.com>
2019-10-10 16:22:21 +0800

    第五次提交：模板定义与继承

---
+++ WeChatApplet/pages/base/base.js
@@ -0,0 +1,74 @@
+// pages/base/base.js
+const app = getApp()
+console.log(app)
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // project: [
+    //   { "name": "素描", "url": "/pages/pencil_sketch/pencil_sketch", "color": "red"},
+    //   { "name": "速写", "url": "/pages/sketch/sketch", "color": "blue" },
+    //   { "name": "色彩", "url": "/pages/color_painting/color_painting", "color": "pink" },
+    //   { "name": "油彩", "url": "/pages/grease_paint/grease_paint", "color": "yellow" },
+    // ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
